f1aab2dc5a0e6e90b4c77d04c42583a9c15e0445
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-30 18:30:20 +0430

    Delete SearchInsertPosition directory

--- SearchInsertPosition/SearchInsertPosition/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using SearchInsertPosition;
-int[] array = { 0, 1, 2, 3, 7, 10, 100 };
-
-Solution solution = new Solution();
-
-var result = solution.RecursiveFind(array, 0, array.Length - 1, 3);
-
-Console.WriteLine(result);
